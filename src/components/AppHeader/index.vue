ff2d0e5b8091e3ecb6afe219d86ded3581492bac
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-11-09 11:11:35 -0300

    use new component AppSearch

--- src/components/AppHeader/index.vue
+++ src/components/AppHeader/index.vue
@@ -1,15 +1,24 @@
+<script setup>
+import AppSearch from '@/components/AppSearch/index.vue';
+</script>
+
 <template>
   <header class="app-header">
     <img alt="Vue logo" class="logo-vue" src="@/assets/logo.svg" width="32" height="32" />
     <img alt="Ubook logo" class="logo-book" src="@/assets/ic-logo.svg" width="148" height="32" />
+    <AppSearch />
   </header>
 </template>
 
 <style lang="scss">
   .app-header {
     padding: 1rem;
+    display: flex;
   }
   .logo-vue {
+    margin-right: 1rem;
+  }
+  .logo-book {
     margin-right: 1rem;
   }
 </style>
