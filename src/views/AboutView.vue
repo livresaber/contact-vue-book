e690099b0da612a0c34dc6df309ebda284bcdb24
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-11-09 09:39:58 -0300

    initial project

---
+++ src/views/AboutView.vue
@@ -0,0 +1,5 @@
+<template>
+  <section class="about">
+    <h1>About page</h1>
+  </section>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="home">  
+    <header>
+      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    </header>
+    <main>
+      <h1>Home page</h1>
+    </main>
+  </section>
+</template>
