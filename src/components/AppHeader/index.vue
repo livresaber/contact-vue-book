68618f8e2d0ad34b3e6e0bd2429633da92461954
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-11-09 10:34:16 -0300

    test(unit): validation logo vue and logo Ubook

--- src/components/AppHeader/index.vue
+++ src/components/AppHeader/index.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="app-header">
     <img alt="Vue logo" class="logo-vue" src="@/assets/logo.svg" width="32" height="32" />
-    <img alt="Ubook" class="logo-book" src="@/assets/ic-logo.svg" width="148" height="32" />
+    <img alt="Ubook logo" class="logo-book" src="@/assets/ic-logo.svg" width="148" height="32" />
   </header>
 </template>
 
--- src/components/AppHeader/test.spec.js
+++ src/components/AppHeader/test.spec.js
@@ -8,4 +8,22 @@ describe('AppHeader', () => {
     const wrapper = mount(AppHeader)
     expect(wrapper.exists()).toBeTruthy()
   })
+
+  it('renders Logo Vue', () => {
+    const wrapper = mount(AppHeader)
+    const logo = wrapper.find('[alt="Vue logo"]')
+
+    expect(logo.exists()).toBeTruthy()
+    expect(logo.element.getAttribute('width')).toBe('32')
+    expect(logo.element.getAttribute('height')).toBe('32')
+  })
+
+  it('renders Logo Ubook', () => {
+    const wrapper = mount(AppHeader)
+    const logo = wrapper.find('[alt="Ubook logo"]')
+
+    expect(logo.exists()).toBeTruthy()
+    expect(logo.element.getAttribute('width')).toBe('148')
+    expect(logo.element.getAttribute('height')).toBe('32')
+  })
 })
