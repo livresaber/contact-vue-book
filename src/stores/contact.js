347869a9632503c3b9b73c8ba8eb3190d2e77aac
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-11-09 22:01:40 -0300

    add logic store crud and getList

--- src/stores/contact.js
+++ src/stores/contact.js
@@ -1,13 +1,52 @@
 import { defineStore } from 'pinia'
+import { useStorage } from '@vueuse/core'
 
 export const useContactStore = defineStore('contact', {
   state: () => ({
-    list: [],
+    list: useStorage('my-contact', []),
+    filter: [],
     modalNewContact: false,
+    modalEditContact: {
+      contact: null,
+      enable: false,
+    },
+    modalDeleteContact: {
+      id: null,
+      enable: false,
+    },
   }),
+  getters: {
+    getListByName: (state) => {
+      return (name) => state.list.filter((contact) => {
+        const regex = new RegExp(name.value, 'i');
+        return contact.name.match(regex);
+      })
+    },
+  },
   actions: {
     newContact(contact) {
       this.list.push(contact)
+    },
+    editContact(contact) {
+      const itemIndex = this.list.findIndex(item => item.id == contact.id)
+      this.list[itemIndex] = contact;
+
+      if(this.filter.length > 0) {
+        const itemIndex = this.filter.findIndex(item => item.id == contact.id)
+        this.filter[itemIndex] = contact;
+      }
+    },
+    deleteContact(id) {
+      this.list = this.list.filter(item => item.id !== id)
+      if(this.filter.length > 0) {
+        this.filter = this.filter.filter(item => item.id !== id)
+      }
+    },
+    setFilter(list) {
+      this.filter = [...list]
+    },
+    resetFilter() {
+      this.filter = []
     },
   },
 })
