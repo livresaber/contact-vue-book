a737330fd3fcfdc6caf6e6d791b061dd1d874093
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-11-10 20:42:54 -0300

    test(unit): test validation appLogo

---
+++ src/components/AppLogo/test.spec.js
@@ -0,0 +1,32 @@
+import { describe, it, expect } from 'vitest'
+
+import { mount } from '@vue/test-utils'
+import AppLogo from './index.vue'
+
+let wrapper;
+
+describe('AppLogo', () => {
+  beforeEach(() => {
+    wrapper = mount(AppLogo)
+  })
+
+  it('renders AppLogo a vue instance', () => {
+    expect(wrapper.exists()).toBeTruthy()
+  })
+
+  it('renders Logo Vue', () => {
+    const logo = wrapper.find('[alt="Vue logo"]')
+
+    expect(logo.exists()).toBeTruthy()
+    expect(logo.attributes('width')).toBe('32')
+    expect(logo.attributes('height')).toBe('32')
+  })
+
+  it('renders Logo Ubook', () => {
+    const logo = wrapper.find('[alt="Ubook logo"]')
+
+    expect(logo.exists()).toBeTruthy()
+    expect(logo.attributes('width')).toBe('148')
+    expect(logo.attributes('height')).toBe('32')
+  })
+})
