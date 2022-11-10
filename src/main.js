b9cb6e8fce5d9a9d4d30a86265ef7fe1c0868bfa
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-11-10 19:50:34 -0300

    refactory file main app

--- src/main.js
+++ src/main.js
@@ -1,17 +1,8 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import Maska from 'maska'
-
 import App from './App.vue'
 import router from './router'
-
 import './assets/main.scss'
 
-const pinia = createPinia()
-const app = createApp(App)
-
-app.use(pinia)
-app.use(router)
-app.use(Maska)
-
-app.mount('#app')
+createApp(App).use(createPinia()).use(router).use(Maska).mount('#app')
