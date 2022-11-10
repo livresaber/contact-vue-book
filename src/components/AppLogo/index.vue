c782bb34f39247e098994874a34052ab8cbbba83
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-11-10 20:43:10 -0300

    component appLogo

---
+++ src/components/AppLogo/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="app-logo" role="img" aria-label="Logos Vue e Ubook">
+    <img alt="Vue logo" class="logo-vue" src="@/assets/logo.svg" width="32" height="32" />
+    <img alt="Ubook logo" class="logo-book" src="@/assets/ic-logo.svg" width="148" height="32" />
+  </div>
+</template>
+
+<style lang="scss">
+  .app-logo {
+    display: flex;
+  }
+  .logo-vue { margin-right: 1rem; }
+  .logo-book { margin-right: 1rem; }
+</style>
