d73eecf7a7bc14f970f65e842a3434f08186e06b
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-11-11 15:05:24 -0300

    test(unit): import beforeEach

--- src/components/AppLogo/test.spec.js
+++ src/components/AppLogo/test.spec.js
@@ -1,4 +1,4 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, beforeEach } from 'vitest'
 
 import { mount } from '@vue/test-utils'
 import AppLogo from './index.vue'
