3b2be5d644333473335148d3f53024ddcf955851
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-11-11 08:52:34 -0300

    test(unit): improvement validation AppAvatar

--- src/components/AppAvatar/test.spec.js
+++ src/components/AppAvatar/test.spec.js
@@ -1,18 +1,32 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, beforeEach } from 'vitest'
 
 import { mount } from '@vue/test-utils'
 import AppAvatar from './index.vue'
 
+let wrapper;
+
 describe('AppAvatar', () => {
-  it('renders AppAvatar a vue instance and prop ariaLabel', () => {
-    const wrapper = mount(AppAvatar, {
+  beforeEach(() => {
+    wrapper = mount(AppAvatar, {
       propsData: {
         ariaLabel: 'Avatar exemplo aria label',
         name: 'lucas',
         color: '#000'
       }
     })
+  })
+  it('renders AppAvatar a vue instance', () => {
     expect(wrapper.exists()).toBeTruthy()
+  })
+  it('renders AppAvatar props', () => {
+    expect(wrapper.props().name).toBe('lucas')
+    expect(wrapper.props().color).toBe('#000')
     expect(wrapper.props().ariaLabel).toBe('Avatar exemplo aria label')
+  })
+  it('renders AppAvatar attributes aria-label', () => {
+    expect(wrapper.attributes('aria-label')).toBe('Avatar exemplo aria label')
+  })
+  it('renders AppAvatar text L', () => {
+    expect(wrapper.text()).toBe('L')
   })
 })
