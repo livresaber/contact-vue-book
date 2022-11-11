2360468a3296eb4e584dbad39810d99449470046
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-11-11 15:14:42 -0300

    whitespace in off config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,7 +17,7 @@ module.exports = {
   ],
   rules: {
     'vue/multi-word-component-names': 'off',
-    'vue/no-mutating-props': 'off '
+    'vue/no-mutating-props': 'off'
   },
   parserOptions: {
     ecmaVersion: 'latest'
