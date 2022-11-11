321cc149a430c8367507dba95d3e61812d2a8897
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-11-11 09:51:42 -0300

    create link for about page

--- src/components/AppLogo/index.vue
+++ src/components/AppLogo/index.vue
@@ -1,13 +1,24 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
 <template>
   <div class="app-logo" role="img" aria-label="Logos Vue e Ubook">
-    <img alt="Vue logo" class="logo-vue" src="@/assets/logo.svg" width="32" height="32" />
-    <img alt="Ubook logo" class="logo-book" src="@/assets/ic-logo.svg" width="148" height="32" />
+    <RouterLink to="/about">
+      <img alt="Vue logo" class="logo-vue" src="@/assets/logo.svg" width="32" height="32" />
+    </RouterLink>
+    <RouterLink to="/">
+      <img alt="Ubook logo" class="logo-book" src="@/assets/ic-logo.svg" width="148" height="32" />
+    </RouterLink>
   </div>
 </template>
 
 <style lang="scss">
   .app-logo {
     display: flex;
+    a {
+      display: flex;
+    }
   }
   .logo-vue { margin-right: 1rem; }
   .logo-book { margin-right: 1rem; }
