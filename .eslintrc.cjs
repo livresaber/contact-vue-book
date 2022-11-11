9a4aa07a0273ae2b30a63ac94a63c09b16f04df9
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-11-11 15:11:19 -0300

    add rules off in lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,6 +15,10 @@ module.exports = {
       ]
     }
   ],
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    'vue/no-mutating-props': 'off '
+  },
   parserOptions: {
     ecmaVersion: 'latest'
   }
