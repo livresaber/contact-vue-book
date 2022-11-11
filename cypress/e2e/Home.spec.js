974a8faa6169645a468d6a60db404c2eefb89ab7
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-11-11 10:39:32 -0300

    test(e2e): validation for create contatcts, edit and delete

--- cypress/e2e/Home.spec.js
+++ cypress/e2e/Home.spec.js
@@ -1,7 +1,49 @@
-describe('Home Page Empty', () => {
-  it('visits home page valid texts empty', () => {
+describe('Home Page', () => {
+  it('content header', () => {
     cy.visit('/')
+    cy.get('[alt="Vue logo"]').should('exist')
+    cy.get('[alt="Ubook logo"]').should('exist')
+    cy.get('[aria-label="Campo de pesquisa para buscar contato"]').should('exist')
+  })
+  it('visits home page valid texts empty', () => {
     cy.contains('Nenhum contato foi criado ainda.')
     cy.contains('Criar contato')
+  })
+  it('create a contact', () => {
+    cy.contains('Criar contato').click()
+    cy.get('input[name="name"]').type('Lucas Ferreira de Lima')
+    cy.get('input[name="email"]').type('lucasferreiralimax@gmail.com')
+    cy.get('input[name="phone"]').type('(82) 99931-9097')
+    cy.contains('Salvar').click()
+
+    cy.contains('Criar contato').click()
+    cy.get('input[name="name"]').type('Elen Souza')
+    cy.get('input[name="email"]').type('elen.souza@clicksign.com')
+    cy.get('input[name="phone"]').type('(11) 94172-8594')
+    cy.contains('Salvar').click()
+
+    cy.contains('Criar contato').click()
+    cy.get('input[name="name"]').type('Criar Art')
+    cy.get('input[name="email"]').type('contato@criar.art')
+    cy.get('input[name="phone"]').type('(82) 99931-9097')
+    cy.contains('Salvar').click()
+  })
+  it('valid contact created', () => {
+    cy.contains('Lucas Ferreira de Lima')
+    cy.contains('lucasferreiralimax@gmail.com')
+    cy.contains('(82) 99931-9097')
+
+    cy.contains('Elen Souza')
+    cy.contains('elen.souza@clicksign.com')
+    cy.contains('(11) 94172-8594')
+  })
+  it('edit contact', () => {
+    cy.get('[aria-label="Botão para edição do contato Lucas Ferreira de Lima"]').click()
+    cy.get('input[name="email"]').clear().type('contato@criar.art')
+    cy.contains('Salvar').click()
+  })
+  it('remove contact', () => {
+    cy.get('[aria-label="Botão para exclusão do contato Criar Art"]').click()
+    cy.get('[aria-label="Botão para excluir o contato selecionado Criar Art"]').click()
   })
 })
