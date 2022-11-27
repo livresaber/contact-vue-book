160247a264fffde47907d2fe78e521c507e6933b
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-11-27 17:38:06 -0300

    new icons

---
+++ src/components/Icons/IconHome.vue
@@ -0,0 +1,7 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" style="enable-background:new 0 0 58.365 58.365"
+    viewBox="0 0 58.365 58.365" width="30" height="30" fill="var(--color-primary)">
+    <path
+      d="M57.863 26.632 29.182 0 .502 26.632a1.001 1.001 0 0 0 1.361 1.466l4.319-4.011v34.278h16v-18a7 7 0 1 1 14 0v18h16V24.087l4.319 4.011a.995.995 0 0 0 .681.267 1 1 0 0 0 .681-1.733z" />
+  </svg>
+</template>
---
+++ src/components/Icons/IconMenu.vue
@@ -0,0 +1,7 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" style="enable-background:new 0 0 56 56"
+    viewBox="0 0 56 56" width="25" height="25" fill="var(--color-primary)">
+    <path
+      d="M28 0C12.561 0 0 12.561 0 28s12.561 28 28 28 28-12.561 28-28S43.439 0 28 0zm12 41H16a2 2 0 0 1 0-4h24a2 2 0 0 1 0 4zm0-11H16a2 2 0 0 1 0-4h24a2 2 0 0 1 0 4zm0-11H16a2 2 0 0 1 0-4h24a2 2 0 0 1 0 4z" />
+  </svg>
+</template>
