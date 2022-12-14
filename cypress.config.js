75203d63e1aee6b9ea8b5cead361d0a8c41e769f
lucasferreiralimax <lucas.lima@interfood.com.br>
2022-12-13 23:33:04 -0300

    add config testIsolation: false

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
-    baseUrl: 'http://localhost:4173'
+    baseUrl: 'http://localhost:4173',
+    testIsolation: false
   }
 })
