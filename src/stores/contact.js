c834cfbf44091b8edbee33058e013eecbe957f7a
lucasferreiralimax <lucasferreiralimax@gmail.com>
2023-03-03 23:56:36 -0300

    add crypto in contact saved localStorage

--- src/stores/contact.js
+++ src/stores/contact.js
@@ -1,9 +1,16 @@
 import { defineStore } from 'pinia'
 import { useStorage } from '@vueuse/core'
+import * as CryptoJS from 'crypto-js'
+import { AES } from 'crypto-js'
 
 export const useContactStore = defineStore('contact', {
   state: () => ({
-    list: useStorage('my-contact', []),
+    list: useStorage('my-contact', [], undefined, {
+      serializer: {
+        read: v => JSON.parse(AES.decrypt(v, import.meta.env.VITE_CONTACT).toString(CryptoJS.enc.Utf8)),
+        write: v => String(AES.encrypt(JSON.stringify(v), import.meta.env.VITE_CONTACT))
+      },
+    }),
     filter: [],
     modalNewContact: false,
     modalEditContact: {
@@ -25,15 +32,15 @@ export const useContactStore = defineStore('contact', {
   },
   actions: {
     newContact(contact) {
-      this.list.push(contact)
+      this.list.push(contact);
     },
     editContact(contact) {
       const itemIndex = this.list.findIndex(item => item.id == contact.id)
-      this.list[itemIndex] = contact;
+      this.list[itemIndex] = contact
 
       if(this.filter.length > 0) {
         const itemIndex = this.filter.findIndex(item => item.id == contact.id)
-        this.filter[itemIndex] = contact;
+        this.filter[itemIndex] = contact
       }
     },
     deleteContact(id) {
