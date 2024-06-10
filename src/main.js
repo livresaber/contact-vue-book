670b38f41d391e5e5daf73fb42344b5cbfccaa60
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-06-10 06:33:23 -0300

    adjustment import vMask

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { vMaska } from "maska"
+import { vMaska } from "maska/vue"
 import App from './App.vue'
 import router from './router'
 import './assets/main.scss'
